a6a605b82f5c2e743df0044237b0e0b70fd43015
Jason Dinh <dinhjdev@gmail.com>
2022-01-21 14:25:32 -0600

    First React Component

--- ppmtool/src/main/java/com/hcl/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/hcl/ppmtool/web/ProjectController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,14 @@ public class ProjectController {
 	
 	@GetMapping("/all")
 	public Iterable<Project> getAllProjects(){return pService.findAllProjects();}
+	
+	@PutMapping("/{id}")
+	public void updateProject(@PathVariable String id, @RequestBody Project project) {
+		Project p = pService.findProjectByIdentifier(id.toUpperCase());
+		p.setProjectName(project.getProjectName());
+		p.setDescription(project.getDescription());
+		pService.saveOrUpdateProject(p);
+	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteProject(@PathVariable String id) {
