1bf57b6da768e51d3b6f3cc6b542ac93fe6f95a7
Jason Dinh <dinhjdev@gmail.com>
2022-01-21 02:57:58 -0600

    Find Project by Identifier

--- ppmtool/src/main/java/com/hcl/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/com/hcl/ppmtool/repositories/ProjectRepository.java
@@ -7,6 +7,5 @@ import com.hcl.ppmtool.domain.Project;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-	@Override
-	Iterable<Project> findAllById(Iterable<Long> iterable);
+	Project findByProjectIdentifier(String projectId);
 }
--- ppmtool/src/main/java/com/hcl/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/hcl/ppmtool/web/ProjectController.java
@@ -1,16 +1,13 @@
 package com.hcl.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +34,12 @@ public class ProjectController {
 		
 		Project project1 = pService.saveOrUpdateProject(project);		
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getProjectById(@PathVariable String id) {
+		Project project = pService.findProjectByIdentifier(id);
+		
+		return new ResponseEntity<Project>(project, HttpStatus.OK);
 	}
 }
