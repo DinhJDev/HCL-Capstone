a2eb1cc0fa247065121e3f457a206f9c39baa504
Jason Dinh <dinhjdev@gmail.com>
2022-01-25 16:13:37 -0600

    Entity Relationships: Project & Backlog

--- ppmtool/src/main/java/com/hcl/ppmtool/repositories/BacklogRepository.java
+++ ppmtool/src/main/java/com/hcl/ppmtool/repositories/BacklogRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.hcl.ppmtool.domain.Backlog;
 
 public interface BacklogRepository extends CrudRepository <Backlog, Long>{
-
+	Backlog findByProjectIdentifier(String Identifier);
 }
