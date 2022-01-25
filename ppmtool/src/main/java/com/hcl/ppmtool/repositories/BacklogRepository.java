4e86b1b83d867cefa1f4dda0576877a23650966d
Jason Dinh <dinhjdev@gmail.com>
2022-01-25 12:41:53 -0600

    Backlog & ProjectTask Entities

---
+++ ppmtool/src/main/java/com/hcl/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.hcl.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hcl.ppmtool.domain.Backlog;
+
+public interface BacklogRepository extends CrudRepository <Backlog, Long>{
+
+}
